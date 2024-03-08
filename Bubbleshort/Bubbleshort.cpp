16510974c92e3eb6301be1fef94cc1f9db563af0
ASUS <ASUS@AZIZDWIMUKTI>
2024-03-08 15:34:39 +0700

    deklarasi variable

--- Bubbleshort/Bubbleshort.cpp
+++ Bubbleshort/Bubbleshort.cpp
@@ -5,4 +5,10 @@ using namespace std;
 int arr(20); // Deklarasi variable global array a dengan ukuran 20
 int n; // Deklarasi variable global n untuk menyimpan banyaknya elemen array
 
+void input() {  //procedur untuk input
 
+	while (true) {
+		cout << "Masukan banyaknya elemen pada array : "; //output ke layar
+		cin >> n; //input dari pengguna
+		if (n <= 20) // jika n kurang dari atau sama dengan 20
+			break; // keluar dari Loop
