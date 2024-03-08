fc616329b31fdd9b5090daf3a567e691d36f4769
ASUS <ASUS@AZIZDWIMUKTI>
2024-03-08 15:40:45 +0700

    input

--- Bubbleshort/Bubbleshort.cpp
+++ Bubbleshort/Bubbleshort.cpp
@@ -12,3 +12,13 @@ void input() {  //procedur untuk input
 		cin >> n; //input dari pengguna
 		if (n <= 20) // jika n kurang dari atau sama dengan 20
 			break; // keluar dari Loop
+		else { // jika n lebih dari 20
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"; // output ke layar
+		}
+	}
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "Masukan Elemen Array" << endl;
+	cout << "====================" << endl;
+
+
