c6eb313c0e4137698b00b549e9b7e980a0cd1de3
ASUS <ASUS@AZIZDWIMUKTI>
2024-03-08 16:28:15 +0700

    output

--- Bubbleshort/Bubbleshort.cpp
+++ Bubbleshort/Bubbleshort.cpp
@@ -27,3 +27,43 @@ void input() {  //procedur untuk input
 
 	}
 }
+void bubbleSortArray() { // procedur untuk mengurutkan array dengan metode bubble sort
+	int pass = 1; // step 1
+
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) { //step 2
+			if (arr[j] > arr[j + 1]) { // step 3
+				int temp;
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+
+			}
+		}
+		pass = pass + 1; //step 4
+
+		cout << "\nPass" << pass - 1 << ": "; // number of pass
+		for (int k = 0; k < n; k++) {
+			cout << arr[k] << " "; // Menampilkan data pada number off pass
+
+		}
+		cout << endl;
+	} while (pass <= n - 1); // step 5
+
+}
+
+void display() {
+	cout << endl;
+	cout << "=================================" << endl;
+	cout << "Element Array yang telah tersusun" << endl;
+	cout << "=================================" << endl;
+	cout << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j]; // menampilkan array
+		if (j < n - 1) {
+			cout << "-->";
+		}
+	}
+
+
+
